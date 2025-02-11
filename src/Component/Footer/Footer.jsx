93179768025303eeef889e465e79b2057b48bce3
BruNah514 <mikemulu713@yahoo.com>
2025-02-11 10:33:56 -0500

    Header and Footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import "./Footer.css";
+import "./footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-
-
 
 const Footer = () => {
   return (
@@ -15,45 +13,41 @@ const Footer = () => {
           <InstagramIcon />
           <YouTubeIcon />
         </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
         </div>
-        <div className="footer_data"> 
-            <div>
-                <ul>
-                    <li>Audio Description</li>
-                    <li>Investor Relations</li>
-                    <li>Legal Notice</li>
-                </ul>
-            </div>
-            <div>
-                <ul>
-                    <li>Help Center</li>
-                    <li>Jobs</li>
-                    <li>Cookie Preferences</li>
-                </ul>
-            </div>
-            <div>
-                <ul>
-                    <li>Gift Cards</li>
-                    <li>Terms of Use</li>
-                    <li>Corporate Information</li>
-                </ul>
-            </div>
+        <div className="service_code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy-right">&copy; 1997-2024 Netflix, Inc.</div>
       </div>
-      <div>
-        <ul>
-            <li>Media Center</li>
-            <li>Privacy</li>
-            <li>Contact Us</li>
-        </ul>
-      </div>
-      <div className='service_code'>
-          <p>
-            Service Code
-          </p>
-        </div>
-        <div className='copy-right'>
-          &copy; 1997-2024 Netflix, Inc.
-        </div>
     </div>
   );
 };
